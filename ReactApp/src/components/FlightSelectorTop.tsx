2c89c22a870b2d34a09a75ffecc46be31ba9dee5
jambi997 <65461882+jambi997@users.noreply.github.com>
2024-02-05 18:59:56 +0100

    feat: svg fix

--- ReactApp/src/components/FlightSelectorTop.tsx
+++ ReactApp/src/components/FlightSelectorTop.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import arrow from "../icons/arrow.svg";
+import arrow from "../icons/Arrow.svg";
 import { colors } from "../data/style";
 import { isMobile } from "react-device-detect";
 
